e00fb3ccb399fec8dbdf36c9f1a557d88ef15ff8
KevinTulakyan <KevinTulakyan@gmail.com>
2021-05-12 14:03:09 -0700

    added cart toggle

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
-
+import Cart from '../components/Cart';
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
@@ -63,6 +63,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
